dad09099447e9bd54dad61953b743c986ee13ef7
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-07-05 22:36:13 -0400

    + filter into all result

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
   def all
     @projects = Project.all.order(created_at: :desc)
+
+    if params[:filter_state].present?
+      @projects = Project.where("state = ?", params[:filter_state])
+    end
   end
 
   def new

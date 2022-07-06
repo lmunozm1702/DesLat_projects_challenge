bfbf83b27ca46d51e0058431b63a2e0714be7dd6
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-07-05 21:28:50 -0400

    + data saved to model + all view displaying all data

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def all
+    @projects = Project.all.order(created_at: :desc)
   end
 
   def new
@@ -7,5 +8,17 @@ class ProjectsController < ApplicationController
   end
 
   def created
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_all_path , notice: 'The project was successfully created'
+    else
+      redirect_to projects_new_path , alert: 'We couldnt create the project, please try again.'
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :start_date, :end_date, :state )
   end
 end
